1c81580ef1c794cd8599baf0b94be0e28877b117
venkat0708 <eswar0077@gmail.com>
2017-06-02 00:39:39 +0530

    categories idex
    assert for page title

--- products/tests/test_functional_products.py
+++ products/tests/test_functional_products.py
@@ -1,20 +1,34 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from test_plus import TestCase
+from time import sleep
 
 
+SLEEP_TIME = 1
 
-
-
-class Test_Category_pages(TestCase):
+class TestCategoryApp(TestCase):
     """To test all the pages related to category management"""
     
     def setUp(self):
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(3)
+        self.browser.maximize_window()
+        self.browser.get('http://localhost:8000/accounts/login')
+        sleep(SLEEP_TIME)
+        
+        
+        username = self.browser.find_element_by_id("id_login")
+        password = self.browser.find_element_by_id("id_password")
+
+        username.send_keys("kvreddy")
+        password.send_keys("vesneven")
+        login_attempt = self.browser.find_element_by_xpath("//*[@type='submit']")
+        login_attempt.submit()
+        sleep(SLEEP_TIME)
         
     def tearDown(self):
         self.browser.close()
         
     def test_index_page(self):
         self.browser.get('http://localhost:8000/products/categories/')
-        assert 'categories' == self.browser.title
+        assert 'Categories' == self.browser.title
