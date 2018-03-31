ba7c2a186fa76229e7cea50be4cea2dda51c4f7c
venkat <eswar0077@gmail.com>
2018-04-01 05:04:02 +0530

    table_sorter

--- config/settings/local.py
+++ config/settings/local.py
@@ -57,7 +57,7 @@ DEBUG_TOOLBAR_CONFIG = {
 
 # django-extensions
 # ------------------------------------------------------------------------------
-INSTALLED_APPS += ['django_extensions', ]
+INSTALLED_APPS += ['django_extensions', 'autofixture' ]
 
 # TESTING
 # ------------------------------------------------------------------------------
